8167206d6b2fac230b9c1b650c1e92869aeb0ae2
hyosang <hyomee@naver.com>
2021-03-22 21:42:49 +0900

    삭제

--- src/main/java/co/kr/knc/core/config/web/CommonInterceptor.java
+++
@@ -1,8 +0,0 @@
-package co.kr.knc.core.config.web;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-@Component
-public class CommonInterceptor extends HandlerInterceptorAdapter {
-}
--- src/main/java/co/kr/knc/online/custmgmt/dto/MyBatisDTO.java
+++
@@ -1,7 +0,0 @@
-package co.kr.knc.online.custmgmt.dto;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public class MyBatisDTO {
-}
